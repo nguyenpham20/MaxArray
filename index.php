cd0341ac5b2d6d755fa012236e826aced2bd4d4c
nguyen <nguyenp021098@gmail.com>
2020-05-12 10:34:48 +0700

    BT

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$one = array
+(
+    array(1,2,3,4,5,6,7),
+    array(3,4,5,6,7,8,9),
+);
+?>
+
+</body>
+</html>
